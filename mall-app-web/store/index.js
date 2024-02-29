80b2a121ccac27906094c00ce2cce144bbc886e5
lijiejun <9890123@qq.com>
2024-02-29 23:10:58 +0800

    2024/2/29 23:10 (桂语江澜)

---
+++ mall-app-web/store/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	state: {
+		hasLogin: false,
+		userInfo: {},
+	},
+	mutations: {
+		login(state, provider) {
+			state.hasLogin = true;
+			state.userInfo = provider;
+			uni.setStorage({
+				key: 'userInfo',
+				data: provider
+			})
+			console.log(state.userInfo);
+		},
+		logout(state) {
+			state.hasLogin = false;
+			state.userInfo = {};
+			uni.removeStorage({
+				key: 'userInfo'
+			});
+			uni.removeStorage({
+				key: 'token'
+			})
+		}
+	},
+	actions: {
+		
+	}
+})
+
+export default store
